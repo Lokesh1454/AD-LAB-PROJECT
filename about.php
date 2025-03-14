fad72198feb5f2f267c8bd1abcade8e6827d1185
Lokesh1454 <157846368+Lokesh1454@users.noreply.github.com>
2025-03-14 16:20:51 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+
+    <!-- swiper css cdn link -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+    <a href="home.php" class="logo">travel.</a>
+
+    <nav class="navbar">
+        <a href="home.php">home</a>
+        <a href="about.php">about</a>
+        <a href="package.php">package</a>
+        <a href="book.php">book</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>    
+<!-- header section ends -->
+
+
+<div class="heading" style="background-image: url(images/header-bg-1.png) no-repeat">
+    <h1>about us</h1>
+</div>
+
+<!-- about section starts -->
+
+<section class="about">
+
+   <div class="image">
+      <img src="images/about.img.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>why choose us?</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ullam sunt omnis exercitationem magnam similique in? Repellendus maiores, nobis omnis dolorum, eos natus dignissimos architecto optio impedit, nemo ducimus adipisci!</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliqud rerum delectus volupate aliqum quaerat iusto repellendus error nulla ab atque.</p>
+      <div class="icons-container">
+        <div class="icons">
+            <i class="fas fa-map"></i>
+            <span>top destinations</span> 
+        </div>
+        <div class="icons">
+            <i class="fas fa-hand-holding-usd"></i>
+            <span>affordable price</span> 
+        </div>
+        <div class="icons">
+            <i class="fas fa-headset"></i>
+            <span>24/7 guide service</span> 
+        </div>
+      </div>
+    </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- review section starts -->
+
+<section class="reviews">
+
+    <div class="swiper reviews-slider">
+      
+       <div class="swiper-wrapper">
+         
+        <div class="swiper-slider slide">
+           <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi cumque libero, culpa architecto optio quos modi sed reprehenderit eveniet neque perspiciatis officia, sint, illo cum ab id obcaecati quam ad!</p>
+            <h3>john deo</h3>
+            <span>traveller</span>
+            <img src="images/pic-1.jpg" alt="">
+        </div>
+
+        <div class="swiper-slider slide">
+           <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, nulla non qui rerum cumque iusto, dicta quae quibusdam neque officia, exercitationem soluta molestias. Nam iste soluta voluptate nesciunt, accusantium fugit accusamus deleniti ad officiis aspernatur aperiam quasi facilis quod amet.</p>
+            <h3>john deo</h3>
+            <span>traveller</span>
+            <img src="images/pic-2.jpg" alt="">
+        </div>
+
+        <div class="swiper-slider slide">
+           <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, nulla non qui rerum cumque iusto, dicta quae quibusdam neque officia, exercitationem soluta molestias. Nam iste soluta voluptate nesciunt, accusantium fugit accusamus deleniti ad officiis aspernatur aperiam quasi facilis quod amet.</p>
+            <h3>john deo</h3>
+            <span>traveller</span>
+            <img src="images/pic-3.jpg" alt="">
+        </div>
+
+        <div class="swiper-slider slide">
+           <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet nobis qui neque, repellat eaque officiis!</p>
+            <h3>john deo</h3>
+            <span>traveller</span>
+            <img src="images/pic-4.jpg" alt="">
+        </div>
+
+        <div class="swiper-slider slide">
+           <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, eaque. Voluptate, ducimus iste expedita doloribus nesciunt eligendi omnis perspiciatis facilis exercitationem quisquam eos asperiores iusto temporibus consequatur illo assumenda dolore quidem. Dignissimos amet labore maxime.</p>
+            <h3>john deo</h3>
+            <span>traveller</span>
+            <img src="images/pic-5.jpg" alt="">
+        </div>
+
+        <div class="swiper-slider slide">
+           <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quos officia totam dolores praesentium est quod maiores nam sunt cum!</p>
+            <h3>john deo</h3>
+            <span>traveller</span>
+            <img src="images/pic-6.jpg" alt="">
+        </div>
+
+       </div>
+
+    </div>
+
+</section>
+
+<!-- review section ends --> 
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- fonter section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+          <h3>quick links</h3>
+          <a href="home.php"><i class="fas.fa-angle-right"></i>home</a>
+          <a href="about.php"><i class="fas.fa-angle-right"></i>about</a>
+          <a href="package.php"><i class="fas.fa-angle-right"></i>package</a>
+          <a href="book.php"><i class="fas.fa-angle-right"></i>book</a>
+        </div>
+
+        <div class="box">
+          <h3>extra links</h3>
+          <a href="#"><i class="fas.fa-angle-right"></i> ask questions</a>
+          <a href="#"><i class="fas.fa-angle-right"></i> about us</a>
+          <a href="#"><i class="fas.fa-angle-right"></i> privacy policy</a>
+          <a href="#"><i class="fas.fa-angle-right"></i> terms of use</a>
+        </div>
+
+        <div class="box">
+          <h3>contact info</h3>
+          <a href="#"><i class="fas.fa-phone"></i> +123-456-7890 </a>
+          <a href="#"><i class="fas.fa-phone"></i> +111-222-3333 </a>
+          <a href="#"><i class="fas.fa-envelope"></i> shaikhanas@gmail.com </a>
+          <a href="#"><i class="fas.fa-map"></i> mumbai, india -400104 </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fas.fa-facebook"></i> facebook </a>
+            <a href="#"><i class="fas.fa-twitter"></i> twitter </a>
+            <a href="#"><i class="fas.fa-instagram"></i> instagram </a>
+            <a href="#"><i class="fas.fa-linkedin"></i> linkedin </a>
+        </div>
+
+    </div>
+
+    <div class="credit">created by <span>mr. web designer</span> | all rights reserved!</div>
+</section>    
+
+<!-- fonter section ends -->
+
+
+
+
+
+
+
+
+
+
+<!-- swiper js link -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+
+<!-- custom js file link -->
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ book_form.php
@@ -0,0 +1,24 @@
+<?php
+
+   $connection = mysqli_connect('localhost','root','','book_db');
+
+   if(isset($POST['send'])){
+       $name = $POST['name'];
+       $email = $POST['email'];
+       $phone = $POST['phone'];
+       $address = $POST['address'];
+       $location = $POST['location'];
+       $guests = $POST['guests'];
+       $arrivals = $POST['arrivals'];
+       $leaving = $POST['leaving'];
+
+       $request = "insert into book_form*(name, email, phone, address, location, guests, arrivals, leaving) values('$name',$email','$phone','$address','$location','$guests','$arrivals','$leaving')";
+
+       mysqli_query($connection,$request);
+
+       header('location:book.php');  
+   }else{
+    echo 'something went wrong try again';
+   }
+
+?>   
